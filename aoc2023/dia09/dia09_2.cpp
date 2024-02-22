043e1e50d29ffe8d8c9c8cd52071771b559c2628
CLozano03 <cesar.lozano.argueso@gmail.com>
2024-02-22 13:36:53 +0100

    Dia09: C++ :star: :star:

---
+++ aoc2023/dia09/dia09_2.cpp
@@ -0,0 +1,54 @@
+/**
+ * Compilar: g++ -o dia09_2 dia09_2.cpp
+ * Ejecutar: ./dia09_2 < input.txt
+ */
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+long next_val_aux(vector<long>& v, vector<long>& next);
+
+/* Comprueba si todos los elementos del vector son 0 */
+bool all_0(const vector<long>& v){
+    for(int i : v){if(i != 0) return false; }
+    return true;
+}
+
+/* Calcula el siguiente vector haciendo la resta de los elementos adyacentes */
+vector<long>& next_vector(vector<long>& v){
+    vector<long>& next = *new vector<long>();
+    for (int i = 0; i < v.size() - 1; i++){
+            next.push_back(v[i + 1] - v[i]);
+    }
+    return next;
+}
+
+long next_val(vector<long>& v){
+    return next_val_aux(v, next_vector(v));
+}
+
+long next_val_aux(vector<long>& v, vector<long>& next){
+    if(all_0(next)) return v.at(0);
+    else return v.at(0) - next_val_aux(next, next_vector(next));
+}
+
+int main() {
+    string linea;
+    int n;
+    long suma_next_vals = 0;
+
+    while(getline(cin, linea)){
+        vector<long> v;
+        istringstream iss(linea);
+        while(iss >> n){
+            v.push_back(n);
+        }
+        suma_next_vals += next_val(v);
+    }
+    cout << "Suma de next_values: " << suma_next_vals << endl;
+
+    return 0;
+}  
