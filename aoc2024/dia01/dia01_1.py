c77b67c56b3d47fd6c37288414e880c2fc6fb647
CLozano03 <cesar.lozano.argueso@gmail.com>
2024-12-04 16:50:43 +0100

    Day01: Py :star: :star:

---
+++ aoc2024/dia01/dia01_1.py
@@ -0,0 +1,21 @@
+
+left = []; right = []
+
+def read_file():
+    with open('input.txt', 'r') as file:
+        for line in file:
+            num1, num2 = map(int, line.split())
+        
+            left.append(num1)
+            right.append(num2)
+            
+
+read_file()
+left.sort()
+right.sort()
+
+sum = 0
+for i in range(len(left)):
+    sum += abs(right[i] - left [i])
+    
+print(f'Total sum of difs: {sum}')
---
+++ aoc2024/dia01/dia01_2.py
@@ -0,0 +1,24 @@
+
+left = []; right_dict = {}
+
+def read_file():
+    with open('input.txt', 'r') as file:
+        for line in file:
+            num1, num2 = map(int, line.split())
+        
+            left.append(num1)
+            
+            if num2 in right_dict:
+                right_dict[num2] += 1
+            else:
+                right_dict[num2] = 1
+
+read_file()
+left.sort()
+
+sum = 0
+for num in left:
+    if num in right_dict: 
+        sum += num * right_dict[num]
+    
+print(f'Total sum of difs: {sum}')
