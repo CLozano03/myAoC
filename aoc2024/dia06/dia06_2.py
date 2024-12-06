a9c83f216792cf9bb1f5529629dc1998e4263226
CLozano03 <cesar.lozano.argueso@gmail.com>
2024-12-06 18:43:22 +0100

    Day06: Py :star: :star:

--- aoc2024/dia06/dia06_2.py
+++ aoc2024/dia06/dia06_2.py
@@ -1,8 +1,10 @@
+import copy
+import time
 from typing import List, Tuple
 
-FILENAME = "input.txt"
+curr_checking_gl = None
 
-visited_coord = []
+FILENAME = "input.txt"
 
 class Map:
     def __init__(self, matrix: List[List[str]]):
@@ -12,6 +14,7 @@ class Map:
         
         self.player_pos = self.__locate_start_pos() # Store player pos
         self.player_dir = 0 # 0: Up 1: Right 2: Down 3: Right
+        self.MAX_VISIT = self.n_cols * self.n_rows
         
     def next_square(self):
         if self.player_dir == 0:
@@ -25,6 +28,12 @@ class Map:
         
     def set_new_pos(self, coord: Tuple[int]):
         self.player_pos = coord
+        
+    def set_obstruction(self, coord: Tuple[int]):
+        self.matrix[coord[0]][coord[1]] = '#'
+    
+    def void_square(self, coord: Tuple[int]):
+        self.matrix[coord[0]][coord[1]] = '.'
     
     def get_el(self, coord: Tuple[int]):
         return self.matrix[coord[0]][coord[1]]
@@ -47,31 +56,66 @@ class Map:
 def read_file(): 
     with open(FILENAME, 'r') as file:
         lines = file.readlines()
-        map_matrix = [list(line) for line in lines]
+        map_matrix = [list(line.strip()) for line in lines]
         
         return Map(matrix = map_matrix)
-
-# Return number of unique visited places
-def run(map: Map):
-    n_visited = 1 # Counting start pos
+    
+# Normal run with no extra obstructions for getting the posible obstructions squares
+def run_normal(map: Map) -> List[Tuple[int]]:
+    visited_coord = []
+    start_pos_map = map.player_pos
     
     while True:
+        if not map.player_pos in visited_coord:
+            visited_coord.append(map.player_pos)
+            
         next = map.next_square()
 
         if not map.belongs_to_matrix(next):
-            return n_visited
+            visited_coord.remove(start_pos_map)
+            return visited_coord
         if map.get_el(next) == '#':
             map.change_dir()
             continue
-        if next not in visited_coord:
-            visited_coord.append(next)
-            n_visited += 1
         
         map.set_new_pos(next)
 
+# Run returns True if goes out of the map and there is no loop
+def run(map: Map) -> bool:
+    n_visited = 0
+    
+    while n_visited < map.MAX_VISIT:        
+        next = map.next_square()
+        
+        if not map.belongs_to_matrix(next):
+            return True
+        if map.get_el(next) == '#':
+            map.change_dir()
+            continue
+        
+        n_visited += 1
+        map.set_new_pos(next)
+
+    return False        
+
+def count_possible_obstructions(start_map: Map):
+    sum_possible_obstrucions = 0
+    
+    mod_map = copy.deepcopy(start_map)
+    possibly_affected = run_normal(mod_map)
+
+    for curr_checking in possibly_affected:
+        mod_map = copy.deepcopy(start_map)        
+        mod_map.set_obstruction(curr_checking)
+                
+        if not run(mod_map):
+            sum_possible_obstrucions += 1
+        
+    return sum_possible_obstrucions
+    
 # -------------------------------------------------------------------------------
 
-map = read_file()
-res = run(map)
+start_map = read_file()
+res = count_possible_obstructions(start_map)
 
-print(f'Sum of unique visited places in the map: {res}')
+print(f'Sum of possible obstructions: {res}')
