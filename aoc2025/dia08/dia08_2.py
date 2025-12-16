4772f7498cf86cbb597aca73da307e9b8159a2a8
César Lozano <clozarch@proton.me>
2025-12-16 22:24:43 +0100

    Dia08: Python :star: :star:

---
+++ aoc2025/dia08/dia08_2.py
@@ -0,0 +1,54 @@
+import heapq
+
+with open("input.txt") as f:
+    points = [line.strip().split(",") for line in f.readlines()]
+
+
+def euclidean_distance(p1, p2):
+    return sum((int(x) - int(y)) ** 2 for x, y in zip(p1, p2))
+
+
+circuits = []
+belongs = {}
+heap = []
+
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        if i != j:
+            dist = euclidean_distance(points[i], points[j])
+            heapq.heappush(heap, (dist, i, j))
+
+
+mul = 0
+while heap:
+    dist, p1, p2 = heapq.heappop(heap)
+
+    c1 = belongs.get(p1, None)
+    c2 = belongs.get(p2, None)
+
+    if c1 is not None and c2 is None:
+        circuits[c1].append(p2)
+        belongs[p2] = c1
+        mul = int(points[p1][0]) * int(points[p2][0])
+
+    if c2 is not None and c1 is None:
+        circuits[c2].append(p1)
+        belongs[p1] = c2
+
+        mul = int(points[p1][0]) * int(points[p2][0])
+
+    if c1 is None and c2 is None:
+        circuits.append([p1, p2])
+        idx = len(circuits) - 1
+        belongs[p1] = idx
+        belongs[p2] = idx
+        mul = int(points[p1][0]) * int(points[p2][0])
+
+    if c1 is not None and c2 is not None and c1 != c2:
+        circuits[c1].extend(circuits[c2])
+        for p in circuits[c2]:
+            belongs[p] = c1
+        circuits[c2] = []
+        mul = int(points[p1][0]) * int(points[p2][0])
+
+print(mul)
